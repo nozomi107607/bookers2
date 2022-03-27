8f1a3a34ba9257bd06bdcb21b98270b32befeccc
EC2 Default User <ec2-user@ip-172-31-25-173.ec2.internal>
2022-03-27 13:29:48 +0000

    Initial Commit

--- db/migrate/20220327125301_add_introduction_to_users.rb
+++ db/migrate/20220327125301_add_introduction_to_users.rb
@@ -1,5 +1,5 @@
 class AddIntroductionToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :introduction：string, :string
+    add_column :users, :introduction, :string
   end
 end
